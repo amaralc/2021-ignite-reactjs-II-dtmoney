da24180713c448c9ca7779e3f42420a974deb0c7
amaralc <amaral.calil@gmail.com>
2021-05-02 20:51:49 -0300

    fix: change url port

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -5,7 +5,7 @@ export function TransactionsTable() {
 
 
   useEffect(() => {
-    fetch('http://localhost:3001/api/transactions')
+    fetch('http://localhost:3000/api/transactions')
       .then(response => response.json())
       .then(data => console.log(data))
   }, [])
