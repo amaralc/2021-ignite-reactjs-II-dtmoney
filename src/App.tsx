ff6b24568259ba4bf52d23edd81e064bee438b96
amaralc <amaral.calil@gmail.com>
2021-05-02 18:59:19 -0300

    feature: add modal and pass function as props

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { createServer } from 'miragejs'
 import { GlobalStyle } from './styles/global'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
 createServer({
   routes() {
@@ -21,12 +23,30 @@ createServer({
   }
 })
 
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+
+      <Modal
+          isOpen={isNewTransactionModalOpen} 
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar Transação</h2>
+        </Modal>
       <GlobalStyle />
     </>
   );
